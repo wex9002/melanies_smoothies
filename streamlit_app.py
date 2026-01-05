eebeb00afb900ae7d083cdeebef1196193327004
wex9002 <98052159+wex9002@users.noreply.github.com>
2026-01-05 11:36:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-
+# new section
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
